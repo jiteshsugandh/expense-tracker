4fa706d1e152d963fef3c465ca2db232d4d0b586
jiteshsugandh <jiteshsugandh@gmail.com>
2021-07-28 18:23:26 +0530

    my first commit react site

---
+++ src/Components/ExpenseTotal.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../Context/AppContext';
+ const ExpenseTotal = () => {
+     const { expenses } = useContext(AppContext);
+
+     const totalExpenses = expenses.reduce((total, item) => {
+         return (total += item.cost);
+     }, 0);
+
+     return(
+         <div className='alert alert-primary'>
+            <span>Spent so far: ₹{totalExpenses} </span> 
+         </div>
+     );
+ };
+
+ export default ExpenseTotal;
